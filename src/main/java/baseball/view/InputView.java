1f1ad419d6a84e8dabda62112219d0955fa07403
wjh2012 <wjh2012@naver.com>
2022-12-07 20:12:18 +0900

    feat(UI): - InputView - OutputView

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,13 @@
+package baseball.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+    public String requestNumber() {
+        return readLine();
+    }
+
+    public String requestRestart() {
+        return readLine();
+    }
+}
