c6e93f00ae61fc2fb6d177e93d1258c3ed48aedc
wjh2012 <wjh2012@naver.com>
2022-12-07 20:11:33 +0900

    test(BaseBallNumberMakerTest): 중복되지 않는 1부터 9사이의 3개 숫자를 생성한다

---
+++ src/test/java/baseball/BaseBallNumberMakerTest.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BaseBallNumberMakerTest {
+
+    @DisplayName("중복되지 않는 1부터 9사이의 3개 숫자를 생성한다")
+    @Test
+    void makeUniqRandomNumber() {
+        // given
+        NumberGenerator numberGenerator = new RandomNumberGenerator();
+        BaseBallNumberMaker baseBallNumberMaker = new BaseBallNumberMaker(numberGenerator);
+
+        // when
+        List<Integer> numbers = baseBallNumberMaker.make();
+        int result = (int) numbers.stream().distinct().count();
+
+        // then
+        Assertions.assertThat(result).isEqualTo(3);
+    }
+}
