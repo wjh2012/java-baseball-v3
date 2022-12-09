bf5cda469f1205b971dd9940b77d4648fae53025
wjh2012 <wjh2012@naver.com>
2022-12-09 23:02:30 +0900

    feat(constants): 재시작 옵션 상수

--- src/main/java/baseball/constants/Constants.java
+++ src/main/java/baseball/constants/Constants.java
@@ -1,6 +1,10 @@
 package baseball.constants;
 
+import java.util.List;
+
 public class Constants {
 
     public static final int GAME_NUMBER_SIZE = 3;
+
+    public static final List<Integer> GAME_RESTART_OPTION = List.of(1, 2);
 }
