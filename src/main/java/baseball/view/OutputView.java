00a84f0398ecb3133955483418441c1b87062633
wjh2012 <wjh2012@naver.com>
2022-12-09 20:07:09 +0900

    refactor(OutputView): 볼, 스트라이크 출력 로직 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import java.util.List;
+
 public class OutputView {
 
     public void greetings() {
@@ -18,12 +20,23 @@ public class OutputView {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    public void ball() {
-        System.out.println("볼");
+    public void ballAndStrike(int ball, int strike){
+        List<String> result = List.of(ball(ball), strike(strike));
+        System.out.println(String.join(" ", result));
     }
 
-    public void strike() {
-        System.out.println("스트라이크");
+    private String ball(int n) {
+        if (n != 0) {
+            return Integer.toString(n);
+        }
+        return "";
+    }
+
+    private String strike(int n) {
+        if (n != 0) {
+            return Integer.toString(n);
+        }
+        return "";
     }
 
     public void nothing() {
