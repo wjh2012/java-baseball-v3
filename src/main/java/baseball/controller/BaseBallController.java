20fb29e288c244c3e80bad2b8dcf078876553b8c
wjh2012 <wjh2012@naver.com>
2022-12-12 16:40:48 +0900

    feat(BaseBallController): 컨트롤러

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -1,18 +1,25 @@
 package baseball.controller;
 
+import static baseball.constants.SelectOption.END;
+import static baseball.constants.SelectOption.isRestart;
+
 import baseball.domain.BaseBallGame;
-import baseball.domain.BaseBallNumberMaker;
+import baseball.view.BaseBallResult;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BaseBallController {
 
+    private final BaseBallGame baseBallGame;
+
     private final OutputView outputView;
     private final InputView inputView;
-    private BaseBallGame baseBallGame;
 
-    public BaseBallController(BaseBallNumberMaker baseBallNumberMaker) {
-        baseBallGame = new BaseBallGame(baseBallNumberMaker);
+    public BaseBallController(BaseBallGame baseBallGame) {
+        this.baseBallGame = baseBallGame;
 
         outputView = new OutputView();
         inputView = new InputView();
@@ -20,10 +27,47 @@ public class BaseBallController {
 
     public void startGame() {
         outputView.greetings();
+        baseBallGame.setComputer();
     }
 
-    public void guessNumber() {
+    public void play() {
+        do {
+            guess();
+        } while (!requestRestart());
+    }
+
+    public void guess() {
+        BaseBallResult result;
+        do {
+            List<Integer> input = inputBaseballNumber();
+            result = baseBallGame.getBallAndStrike(input);
+            printResult(result);
+        } while (!baseBallGame.isOver(result.getStrike()));
+    }
+
+    private boolean requestRestart() {
+        outputView.requestRestart();
+        String input = inputView.requestRestart();
+        return isRestart(input) == END;
+    }
+
+    private List<Integer> inputBaseballNumber() {
         outputView.requestNumber();
-        inputView.requestNumber();
+        String input = inputView.requestNumber();
+        try {
+            return convertNumbers(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 입력", e);
+        }
+    }
+
+    private List<Integer> convertNumbers(String input) {
+        return Stream.of(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private void printResult(BaseBallResult result) {
+        outputView.result(result);
     }
 }
